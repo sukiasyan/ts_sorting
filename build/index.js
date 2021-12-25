ab642a8afe977a850e99145713770a0e37e3edb8
hsukiasyan <h.sukiasyan@gethatch.com>
2021-12-25 22:36:35 +0100

    seperate classes

--- build/index.js
+++ build/index.js
@@ -1,26 +1,8 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-            }
-            if (typeof this.collection === 'string') {
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var NumberCollection_1 = require("./NumberCollection");
+var numberCollection = new NumberCollection_1.NumberCollection([10, 3, -5, 0, 7]);
+var sorter = new Sorter_1.Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter';
+import { NumberCollection } from './NumberCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i; j++) {
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-      }
-
-      if (typeof this.collection === 'string') {
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numberCollection = new NumberCollection([10, 3, -5, 0, 7]);
+const sorter = new Sorter(numberCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numberCollection.data);
