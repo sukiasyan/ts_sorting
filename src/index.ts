1ae2fcf72139c391342d4959f4c098a2f1b4a34c
hsukiasyan <h.sukiasyan@gethatch.com>
2021-12-26 21:34:38 +0100

    abstract class for sorter class

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('Xaayb');
+
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -19,6 +19,5 @@ linkedList.add(10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
